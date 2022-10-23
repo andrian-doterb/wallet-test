c264d10d94b7acd8148d628f8feb5527354644f1
andrian <andrian.rizki@doterb.com>
2022-10-23 13:40:31 +0700

    Initial Setup

---
+++ config/importmap.rb
@@ -0,0 +1,9 @@
+# Pin npm packages by running ./bin/importmap
+
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin '@popperjs/core', to: 'popper.min.js' # @2.11.6
+pin 'bootstrap', to: 'bootstrap.min.js' # @5.2.2
